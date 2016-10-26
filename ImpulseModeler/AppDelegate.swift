8cf1a8914497f04c45cb4d11a93a2ce5ec2339f0
PeterCoolAssHuber <peterhub@peteserver.local>
2016-10-26 16:37:56 -0400

    Initial Commit

---
+++ ImpulseModeler/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  ImpulseModeler
+//
+//  Created by PeterCoolAssHuber on 2016-10-26.
+//  Copyright © 2016 Peter Huber. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
